5b0dc93f456f12aeb3ec5eb2212a1eb1b122f9da
Julien Dorra <juliendorra@juliendorra.com>
2025-04-22 11:10:59 +0200

    good values to generate cards

---
+++ generate_cards.sh
@@ -0,0 +1,5 @@
+# recommanded settings for generating cards
+
+deno run --allow-read --allow-write generate_terrain_cards.ts --iconSize=120
+
+deno run --allow-read --allow-write --allow-net generate_unit_cards.ts --marginVertical=50 --fontSizeName=18 --fontSizeStats=16 --marginHorizontal=40  
