9c370d4d12f024767c35857ca0c950c195860c0a
Julien Dorra <juliendorra@juliendorra.com>
2025-04-24 19:09:48 +0200

    bigger text on cards

--- generate_cards.sh
+++ generate_cards.sh
@@ -2,7 +2,7 @@
 
 deno run --allow-read --allow-write generate_terrain_cards.ts --iconSize=120
 
-deno run --allow-read --allow-write --allow-net generate_unit_cards.ts --marginVertical=50 --fontSizeName=18 --fontSizeStats=16 --marginHorizontal=40  
+deno run --allow-read --allow-write --allow-net generate_unit_cards.ts --marginVertical=50 --fontSizeName=22 --fontSizeStats=28 --marginHorizontal=40  
 
 deno run --allow-read --allow-write --allow-net compress_images.ts
 
